04162c1c4e103fb0fabef334c9612b0b385df76e
elijah421 <81763567+elijah421@users.noreply.github.com>
2024-04-29 17:52:01 +0800

    Delete gym1 directory

--- gym1/src/main/resources/static/appointment.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var navbar = document.getElementById("navbar");
-    var navControl = document.getElementById("nav-control");
-
-    navControl.addEventListener("click", function() {
-        navbar.classList.toggle("show");
-    });
-});
