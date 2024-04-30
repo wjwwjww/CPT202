ff224c0c0d48675e5fe6c2c53b44c8b7709f41bc
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2024-04-30 18:33:00 +0800

    Update CustomerRegisterRepo.java

--- gym1/src/main/java/com/gym1/gym1/Repository/CustomerRegisterRepo.java
+++ gym1/src/main/java/com/gym1/gym1/Repository/CustomerRegisterRepo.java
@@ -1,9 +1,9 @@
-package com.register.customer.repository;
+package com.gym1.gym1.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.register.customer.model.CustomerRegister;
+import com.gym1.gym1.Model.CustomerRegister;
 
 @Repository
 public interface CustomerRegisterRepo extends JpaRepository<CustomerRegister, String> {
