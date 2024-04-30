a3171da3c72f360037c8c6bd8ba415953fb2bef4
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2024-04-30 18:35:48 +0800

    Update PurchasedPlanRepo.java

--- gym1/src/main/java/com/gym1/gym1/Repository/PurchasedPlanRepo.java
+++ gym1/src/main/java/com/gym1/gym1/Repository/PurchasedPlanRepo.java
@@ -1,9 +1,9 @@
-package com.register.customer.repository;
+package com.gym1.gym1.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.register.customer.model.PurchasedPlan;
+import com.gym1.gym1.Model.PurchasedPlan;
 
 @Repository
 public interface PurchasedPlanRepo extends JpaRepository<PurchasedPlan, String> {
