a9fea13ef2ea7a0b24cd9205ac445c94fed1a5fa
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2024-05-04 21:42:54 +0800

    Update PurchasedPlan.java

--- gym1/src/main/java/com/gym1/gym1/Model/PurchasedPlan.java
+++ gym1/src/main/java/com/gym1/gym1/Model/PurchasedPlan.java
@@ -1,5 +1,7 @@
 package com.gym1.gym1.Model;
 
+import java.util.Date;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -11,30 +13,41 @@ import jakarta.persistence.OneToOne;
 @Table(name = "purchased_plan")
 public class PurchasedPlan {
     @Id
-    private String email;
+    private Integer userId;
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "email", referencedColumnName = "email", insertable = false, updatable = false)
-    private PayingCustomer payingCustomer;
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private Customer customer;
 
     private String planName;
     private String duration;
     private String price;
+    private String status;
+    private Date purchaseDate;
+    private Date expiryDate;
 
-    public String getEmail() {
-        return email;
+    public Date getPurchaseDate() {
+        return purchaseDate;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
     }
 
-    public PayingCustomer getPayingCustomer() {
-        return payingCustomer;
+    public Integer getUserId() {
+        return userId;
     }
 
-    public void setPayingCustomer(PayingCustomer payingCustomer) {
-        this.payingCustomer = payingCustomer;
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 
     public String getPlanName() {
@@ -61,4 +74,26 @@ public class PurchasedPlan {
         this.price = price;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public void setEmail(String email) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setEmail'");
+    }
+
 }
+
